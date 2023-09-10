c76290fc8c4ec83bfeb97e401dcc7f96bb0d0998
sejarparvez <sejarparvez@yahoo.com>
2023-09-10 08:12:57 +0600

    added neovim config

---
+++ nvim/lua/plugins/nightfly.lua
@@ -0,0 +1,12 @@
+return {
+
+  { "bluz71/vim-nightfly-colors" },
+
+  -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "nightfly",
+  --   },
+  -- },
+}
---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+
+  { "folke/tokyonight.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
